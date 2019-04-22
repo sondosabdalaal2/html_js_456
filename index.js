8e357e892a2048e1c643e2e58f20be0af43a5957
sondosabdalaal2 <dodo9393@hotmail.com>
2019-04-22 14:05:35 +0300

    ee

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 //onsole.log("this is text");
 let pElement = document.getElementById('paragraph1');
 pElement.innerHTML='inner html has been changed';
+let p2= document.getElementById('paraghragh2')
+p2.innerHTML='parag is edited'
